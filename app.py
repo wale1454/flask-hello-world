78fb8a67ecefd61bbb209d22cd7d2d6f8c9a1cac
Wale Olowonyo F <31905212+wale1454@users.noreply.github.com>
2023-03-28 14:45:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import os
+
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World Test !!'
+    
+    my_variable_value = os.getenv('TEST_APIKEY', 'default_value')
+
+    
+    return my_variable_value
